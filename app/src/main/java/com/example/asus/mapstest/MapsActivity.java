a48f05fab67a168bebbcdbc66591a077f01dfe8b
ryansat <ryansat46@gmail.com>
2018-06-09 22:14:14 +0700

    add update location
    add update location

--- app/src/main/java/com/example/asus/mapstest/MapsActivity.java
+++ app/src/main/java/com/example/asus/mapstest/MapsActivity.java
@@ -1,50 +1,61 @@
 package com.example.asus.mapstest;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.support.annotation.NonNull;
-import android.support.annotation.RequiresApi;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
+import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.LatLngBounds;
 
 public class MapsActivity extends FragmentActivity implements GoogleMap.OnMyLocationButtonClickListener, GoogleMap.OnMyLocationClickListener,
-        OnMapReadyCallback {
+        OnMapReadyCallback, LocationListener,GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
 
     private static final int MY_LOCATION_REQUEST_CODE = 99;
-    private GoogleMap mMap;
+    public GoogleMap mMap;
+    //public static MapFragment mapFragment;
     public static final int MY_PERMISSIONS_REQUEST_LOCATION = 101;
     public Location lokasi;
     int requestCode;
     String[] permissions;
     int[] grantResults;
+    private LocationManager locationManager;
+    private String provider;
+    public float lat;
+    public float lng;
+    //private FusedLocationProviderClient mFusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        //onRequestPermissionsResult(99,"",1);
-
-        //onRequestPermissionsResult(99,"",grantResults[0]);
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
 
         final Button button = findViewById(R.id.btn1);
         button.setOnClickListener(new View.OnClickListener() {
@@ -52,10 +63,31 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMyLoca
                 double longitude = lokasi.getLongitude();
                 double latitude = lokasi.getLatitude();
                 //lokasi = location;
-                Toast.makeText(getApplicationContext(), "Longitude : "+longitude+"\n Latitude : "+latitude, Toast.LENGTH_LONG).show();
-
+                //Toast.makeText(getApplicationContext(), "Longitude : " + longitude + "\n Latitude : " + latitude, Toast.LENGTH_LONG).show();
+                float lat = (float) (lokasi.getLatitude());
+                float lng = (float) (lokasi.getLongitude());
+                Log.i("Latitude", "Lattitude:" +lat);
+                Log.i("Longitude", "Longitude:" +lng);
+                LatLng latLng = new LatLng(lat,lng);
+                CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 10);
+                mMap.animateCamera(cameraUpdate);
             }
         });
+       // onMyLocationButtonClick();
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, false);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(provider);
+        if (location != null) {
+            System.out.println("Provider " + provider + " has been selected.");
+            onLocationChanged(location);
+        } else {
+            System.out.println("Location not avilable");
+        }
+
 
     }
 
@@ -68,27 +100,31 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMyLoca
         mMap = map;
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             mMap.setMyLocationEnabled(true);
-        }
-        else{
+        } else {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},MY_PERMISSIONS_REQUEST_LOCATION);
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_LOCATION);
             }
         }
-            mMap.setOnMyLocationButtonClickListener(this);
-            mMap.setOnMyLocationClickListener(this);
-        }
+        mMap.setOnMyLocationButtonClickListener(this);
+        mMap.setOnMyLocationClickListener(this);
+        Toast.makeText(getApplicationContext(), lat+"----"+lng,Toast.LENGTH_LONG).show();
+        Log.i("Latitude", "Lattitude:" +lat);
+        Log.i("Longitude", "Longitude:" +lng);
+        LatLng latLng = new LatLng(lat,lng);
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 17);
+        mMap.animateCamera(cameraUpdate);
+    }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode){
+        switch (requestCode) {
             case MY_PERMISSIONS_REQUEST_LOCATION:
-                if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
                         mMap.setMyLocationEnabled(true);
                     }
-                }
-                else {
+                } else {
                     Toast.makeText(this, "This Apps Need Permision", Toast.LENGTH_LONG).show();
                     finish();
                 }
@@ -97,25 +133,86 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMyLoca
     }
 
     @Override
-        public void onMyLocationClick(@NonNull Location location) {
-        //Toast.makeText(this, "Current location:\n" + location, Toast.LENGTH_LONG).show();
-        //LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-        //Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+    public void onMyLocationClick(@NonNull Location location) {
         double longitude = location.getLongitude();
         double latitude = location.getLatitude();
         lokasi = location;
-        Toast.makeText(this, "Current longitude:\n" + longitude+ "\n Current latitude:\n"+latitude, Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Current longitude:\n" + longitude + "\n Current latitude:\n" + latitude, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        Toast.makeText(this, "Get Location", Toast.LENGTH_SHORT).show();
+        return false;
+    }
+
+
+    protected void onResume() {
+        super.onResume();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            return;
         }
+        locationManager.requestLocationUpdates(provider, 400, 1, (LocationListener) this);
+    }
 
-        @Override
-        public boolean onMyLocationButtonClick() {
-            Toast.makeText(this, "Get Location", Toast.LENGTH_SHORT).show();
-            // Return false so that we don't consume the event and the default behavior still occurs
-            // (the camera animates to the user's current position).
-           // mMap.onMyLocationClick(this);
-
-            return false;
-        }
-
+    /* Remove the locationlistener updates when Activity is paused */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        locationManager.removeUpdates((LocationListener) this);
 
     }
+
+
+    public void onLocationChanged(Location location) {
+
+        lat = (float) (location.getLatitude());
+        lng = (float) (location.getLongitude());
+        Toast.makeText(getApplicationContext(), lat+"----"+lng,Toast.LENGTH_LONG).show();
+        Log.i("Latitude", "Lattitude:" +lat);
+        Log.i("Longitude", "Longitude:" +lng);
+        LatLng latLng = new LatLng(lat,lng);
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 17);
+    }
+
+
+
+    @Override
+    public void onProviderDisabled(String arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onProviderEnabled(String arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+        // TODO Auto-generated method stub
+
+    }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
+        //Location location = mLocationClient.getLastLocation();
+        LatLng latLng = new LatLng(lokasi.getLatitude(), lokasi.getLongitude());
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 17);
+        mMap.animateCamera(cameraUpdate);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+}
